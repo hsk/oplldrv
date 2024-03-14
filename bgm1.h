d1ff4888c6b7324b320b86ee474fa4283ae66ae7
hsk <sakurai777@gmail.com>
2024-03-14 22:16:57 +0900

    fix

--- bgm1.h
+++ bgm1.h
@@ -1,19 +1,16 @@
 /*
-# ['opll_mode 1', 'tempo 77 ', 'title {"RUINS OF MOONDORIA"}']
-g opll_mode 1
-g tempo 77 
-g title {"RUINS OF MOONDORIA"}
+# []
 k 9:vs []
 k A:vs []
 k B:vs []
-k C:vs ['l8v14@2q2o3<[a+24]60']
-k D:vs ['l24v14@2q2o6<', 'a+a+a+a+a+a+a+a+a+a+', 'a+a+a+a+a+a+a+a+a+a+', 'a+a+a+a+a+a+a+a+a+a+', 'a+a+a+a+a+a+a+a+a+a+', 'a+a+a+a+a+a+a+a+a+a+', 'a+a+a+a+a+a+a+a+a+a+']
-k E:vs []
-k F:vs []
+k C:vs []
+k D:vs ['t280o5@4v15l8q7', 'a1r4[ea]3>c+e1^2r4^8<', 'a+1r4[f+a+]3>c+f+1^2r4^8<', 't240>d1<r4[a>d<]3>f+a1^2r4^8', 't180e4^8>e8^2^1^1r1']
+k E:vs ['o4@6v15l8q5', 'a1r4[ea]3>c+e1^2r4^8<', 'a+1r4[f+a+]3>c+f+1^2r4^8<', '>d1<r4[a>d<]3>f+a1^2r4^8', 'e4^8>e8^2^1^1r1']
+k F:vs ['o2@14v15l4q6a1^1^1^2g+2', 'f+1^1^1f+2c+2d1^1^1^1e1^1^1r1']
 k G:vs []
 k H:vs []
-{'@': [], '#': ['opll_mode 1', 'tempo 77 ', 'title {"RUINS OF MOONDORIA"}'], '9': '', 'A': '', 'B': '', 'C': 'l8v14@2q2o3<[a+24]60', 'D': 'l24v14@2q2o6<a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+a+', 'E': '', 'F': '', 'G': '', 'H': ''}
-{'@': {}, '#': {'opll_mode': 1, 'tempo': 77, 'title': 'RUINS OF MOONDORIA'}, '9': [], 'A': [], 'B': [], 'C': [['l', 24.0], ['v', 14], ['@', 2], ['q', 2], ['o', 3], ['<'], ['['], ['tone', 'a+', 8.0], [']', 60]], 'D': [['l', 8.0], ['v', 14], ['@', 2], ['q', 2], ['o', 6], ['<'], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0], ['tone', 'a+', 8.0]], 'E': [], 'F': [], 'G': [], 'H': []}
+{'@': [], '#': [], '9': '', 'A': '', 'B': '', 'C': '', 'D': 't280o5@4v15l8q7a1r4[ea]3>c+e1^2r4^8<a+1r4[f+a+]3>c+f+1^2r4^8<t240>d1<r4[a>d<]3>f+a1^2r4^8t180e4^8>e8^2^1^1r1', 'E': 'o4@6v15l8q5a1r4[ea]3>c+e1^2r4^8<a+1r4[f+a+]3>c+f+1^2r4^8<>d1<r4[a>d<]3>f+a1^2r4^8e4^8>e8^2^1^1r1', 'F': 'o2@14v15l4q6a1^1^1^2g+2f+1^1^1f+2c+2d1^1^1^1e1^1^1r1', 'G': '', 'H': ''}
+{'@': {}, '#': {'opll_mode': 0}, '9': [], 'A': [], 'B': [], 'C': [], 'D': [['t', 280], ['o', 5], ['@', 4], ['v', 15], ['l', 24.0], ['q', 7], ['tone', 'a', 192.0], ['tone', 'r', 48.0], ['['], ['tone', 'e', 24.0], ['tone', 'a', 24.0], [']', 3], ['>'], ['tone', 'c+', 24.0], ['tone', 'e', 288.0], ['tone', 'r', 72.0], ['<'], ['tone', 'a+', 192.0], ['tone', 'r', 48.0], ['['], ['tone', 'f+', 24.0], ['tone', 'a+', 24.0], [']', 3], ['>'], ['tone', 'c+', 24.0], ['tone', 'f+', 288.0], ['tone', 'r', 72.0], ['<'], ['t', 240], ['>'], ['tone', 'd', 192.0], ['<'], ['tone', 'r', 48.0], ['['], ['tone', 'a', 24.0], ['>'], ['tone', 'd', 24.0], ['<'], [']', 3], ['>'], ['tone', 'f+', 24.0], ['tone', 'a', 288.0], ['tone', 'r', 72.0], ['t', 180], ['tone', 'e', 72.0], ['>'], ['tone', 'e', 504.0], ['tone', 'r', 192.0]], 'E': [['o', 4], ['@', 6], ['v', 15], ['l', 24.0], ['q', 5], ['tone', 'a', 192.0], ['tone', 'r', 48.0], ['['], ['tone', 'e', 24.0], ['tone', 'a', 24.0], [']', 3], ['>'], ['tone', 'c+', 24.0], ['tone', 'e', 288.0], ['tone', 'r', 72.0], ['<'], ['tone', 'a+', 192.0], ['tone', 'r', 48.0], ['['], ['tone', 'f+', 24.0], ['tone', 'a+', 24.0], [']', 3], ['>'], ['tone', 'c+', 24.0], ['tone', 'f+', 288.0], ['tone', 'r', 72.0], ['<'], ['>'], ['tone', 'd', 192.0], ['<'], ['tone', 'r', 48.0], ['['], ['tone', 'a', 24.0], ['>'], ['tone', 'd', 24.0], ['<'], [']', 3], ['>'], ['tone', 'f+', 24.0], ['tone', 'a', 288.0], ['tone', 'r', 72.0], ['tone', 'e', 72.0], ['>'], ['tone', 'e', 504.0], ['tone', 'r', 192.0]], 'F': [['o', 2], ['@', 14], ['v', 15], ['l', 48.0], ['q', 6], ['tone', 'a', 672.0], ['tone', 'g+', 96.0], ['tone', 'f+', 576.0], ['tone', 'f+', 96.0], ['tone', 'c+', 96.0], ['tone', 'd', 768.0], ['tone', 'e', 576.0], ['tone', 'r', 192.0]], 'G': [], 'H': []}
 */
 u8 const bgm1_0[2]={
   0,PEND};
@@ -21,16 +18,16 @@ u8 const bgm1_1[2]={
   0,PEND};
 u8 const bgm1_2[2]={
   0,PEND};
-u8 const bgm1_3[16]={
-  1,PLOOP,30,60,PVOLUME,49,/*PTONE,*/22,1,PKEYOFF,6,PNEXT,246,255,47,60,PEND};
-u8 const bgm1_4[244]={
-  0,PVOLUME,49,/*PTONE,*/58,1,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,5,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,5,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,5,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,5,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,1,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,5,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,5,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,5,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,5,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,1,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,5,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,5,/*PTONE,*/58,2,PKEYOFF,6,/*PTONE,*/58,2,PKEYOFF,6,PEND};
-u8 const bgm1_5[2]={
+u8 const bgm1_3[2]={
   0,PEND};
-u8 const bgm1_6[2]={
-  0,PEND};
+u8 const bgm1_4[106]={
+  1,PVOLUME,80,/*PTONE,*/57,45,PKEYOFF,6,PWAIT,13,PLOOP,1,3,/*PTONE,*/52,5,PKEYOFF,1,/*PTONE,*/57,6,PNEXT,246,255,2,3,/*PTONE,*/61,6,PKEYOFF,1,/*PTONE,*/64,67,PKEYOFF,10,PWAIT,19,/*PTONE,*/58,45,PKEYOFF,7,PWAIT,13,PLOOP,1,3,/*PTONE,*/54,5,PKEYOFF,1,/*PTONE,*/58,6,PNEXT,246,255,2,3,/*PTONE,*/61,6,PKEYOFF,1,/*PTONE,*/66,67,PKEYOFF,10,PWAIT,19,/*PTONE,*/62,52,PKEYOFF,8,PWAIT,15,PLOOP,1,3,/*PTONE,*/57,6,PKEYOFF,1,/*PTONE,*/62,7,PKEYOFF,1,PNEXT,244,255,0,3,/*PTONE,*/66,6,PKEYOFF,1,/*PTONE,*/69,79,PKEYOFF,11,PWAIT,23,/*PTONE,*/64,26,PKEYOFF,4,/*PTONE,*/76,184,PKEYOFF,26,PWAIT,80,PEND};
+u8 const bgm1_5[110]={
+  1,PVOLUME,112,/*PTONE,*/45,32,PKEYOFF,19,PWAIT,13,PLOOP,1,3,/*PTONE,*/40,4,PKEYOFF,2,/*PTONE,*/45,4,PKEYOFF,2,PNEXT,244,255,2,3,/*PTONE,*/49,4,PKEYOFF,3,/*PTONE,*/52,48,PKEYOFF,29,PWAIT,19,/*PTONE,*/46,32,PKEYOFF,20,PWAIT,13,PLOOP,1,3,/*PTONE,*/42,4,PKEYOFF,2,/*PTONE,*/46,4,PKEYOFF,2,PNEXT,244,255,2,3,/*PTONE,*/49,4,PKEYOFF,3,/*PTONE,*/54,48,PKEYOFF,29,PWAIT,19,/*PTONE,*/50,37,PKEYOFF,23,PWAIT,15,PLOOP,1,3,/*PTONE,*/45,4,PKEYOFF,3,/*PTONE,*/50,5,PKEYOFF,3,PNEXT,244,255,0,3,/*PTONE,*/54,5,PKEYOFF,2,/*PTONE,*/57,57,PKEYOFF,33,PWAIT,23,/*PTONE,*/52,19,PKEYOFF,11,/*PTONE,*/64,131,PKEYOFF,79,PWAIT,80,PEND};
+u8 const bgm1_6[34]={
+  0,PVOLUME,240,/*PTONE,*/21,135,PKEYOFF,45,/*PTONE,*/20,19,PKEYOFF,6,/*PTONE,*/18,116,PKEYOFF,39,/*PTONE,*/18,19,PKEYOFF,6,/*PTONE,*/13,20,PKEYOFF,6,/*PTONE,*/14,180,PKEYOFF,60,/*PTONE,*/16,180,PKEYOFF,60,PWAIT,80,PEND};
 u8 const bgm1_7[2]={
   0,PEND};
 u8 const bgm1_8[2]={
   0,PEND};
-u8* const bgm1[]={(u8*)265,NULL,bgm1_0,bgm1_1,bgm1_2,bgm1_3,bgm1_4,bgm1_5,bgm1_6,bgm1_7,bgm1_8,};
+u8* const bgm1[]={(u8*)9,NULL,bgm1_0,bgm1_1,bgm1_2,bgm1_3,bgm1_4,bgm1_5,bgm1_6,bgm1_7,bgm1_8,};
